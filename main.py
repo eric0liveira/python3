a9117c7e1172fc10370ebd7a878172b3e91147e6
Eric Oliveira <eric.erikito@gmail.com>
2018-06-20 16:50:24 -0300

    exemplos de atribuicao e tipo de variável

---
+++ main.py
@@ -0,0 +1,10 @@
+nome = "eric"
+idade = 25
+sobrenome = "rodrigues"
+
+print(nome, sobrenome, idade)
+print("ola mundo", nome)
+print(f"olá, {nome} {sobrenome}, sua idade é {idade}")
+
+print(type(nome))
+print(type(idade))
