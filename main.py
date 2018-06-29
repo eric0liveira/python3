26c7e88b43e77208b630dc8427004554433b62f0
Eric Oliveira <eric.erikito@gmail.com>
2018-06-29 16:55:24 -0300

    exercicios python 1001 - 1010

--- main.py
+++ main.py
@@ -8,3 +8,13 @@ print(f"olá, {nome} {sobrenome}, sua idade é {idade}")
 
 print(type(nome))
 print(type(idade))
+
+nome = int(input("Digite seu nome: "))
+
+print("ola mundo", nome)
+
+print(4//3)
+print(5//3)
+print(7//3)
+print(8//3)
+print(10//3)
